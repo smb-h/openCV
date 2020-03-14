8d385a4b80b599157e48c5413684e5baae0293fd
smbh <smb_h@yahoo.com>
2020-03-14 19:49:20 +0330

    minor changes

--- glasses_and_stash.py
+++ glasses_and_stash.py
@@ -10,12 +10,12 @@ save_path           = 'saved-media/glasses_and_stash.mp4'
 frames_per_seconds  = 24
 config              = CFEVideoConf(cap, filepath=save_path, res='720p')
 out                 = cv2.VideoWriter(save_path, config.video_type, frames_per_seconds, config.dims)
-# face_cascade        = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
-face_cascade        = cv2.CascadeClassifier(cv2.data.haarcascades + "cascades/data/haarcascade_frontalface_default.xml")
-# eyes_cascade        = cv2.CascadeClassifier('cascades/3rd-party/frontalEyes35x16.xml')
-eyes_cascade        = cv2.CascadeClassifier(cv2.data.haarcascades + 'cascades/3rd-party/frontalEyes35x16.xml')
-# nose_cascade        = cv2.CascadeClassifier('cascades/3rd-party/Nose18x15.xml')
-nose_cascade        = cv2.CascadeClassifier(cv2.data.haarcascades + 'cascades/3rd-party/Nose18x15.xml')
+face_cascade        = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_default.xml')
+# face_cascade        = cv2.CascadeClassifier(cv2.data.haarcascades + "cascades/data/haarcascade_frontalface_default.xml")
+eyes_cascade        = cv2.CascadeClassifier('cascades/3rd-party/frontalEyes35x16.xml')
+# eyes_cascade        = cv2.CascadeClassifier(cv2.data.haarcascades + 'cascades/3rd-party/frontalEyes35x16.xml')
+nose_cascade        = cv2.CascadeClassifier('cascades/3rd-party/Nose18x15.xml')
+# nose_cascade        = cv2.CascadeClassifier(cv2.data.haarcascades + 'cascades/3rd-party/Nose18x15.xml')
 glasses             = cv2.imread("images/fun/glasses.png", -1)
 mustache            = cv2.imread('images/fun/mustache.png',-1)
 
